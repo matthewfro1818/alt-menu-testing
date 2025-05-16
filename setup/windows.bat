c3712c55a187190b87a26ae83335c90106cf0e26
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2025-05-16 18:45:13 -0400

    Update windows.bat

--- setup/windows.bat
+++ setup/windows.bat
@@ -5,8 +5,8 @@ echo Installing dependencies, please wait...
 haxelib install hxcpp
 haxelib install lime 8.1.2
 haxelib install openfl 9.3.3
-haxelib install flixel 5.6.1
-haxelib install flixel-addons 3.3.0
+haxelib install flixel 4.11.0
+haxelib install flixel-addons 2.11.0
 haxelib install flixel-tools
 haxelib install flixel-ui 2.6.1
 haxelib install actuate 1.9.0 
@@ -19,7 +19,7 @@ haxelib install extension-webm
 haxelib git hscript-ex https://github.com/ianharrigan/hscript-ex
 haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
 haxelib git hxvm-luajit https://github.com/nebulazorua/hxvm-luajit
-haxelib git faxe https://github.com/uhrobots/faxe
+haxelib git faxe https://github.com/uhrobots/faxea
 haxelib git polymod https://github.com/larsiusprime/polymod.git
 haxelib install hxcpp-debug-server
 haxelib list
