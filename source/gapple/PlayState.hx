9990d72afd0b97a9c5577065328d49d6a35e8b1c
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2025-05-17 10:57:50 -0400

    Update PlayState.hx

--- source/gapple/PlayState.hx
+++ source/gapple/PlayState.hx
@@ -886,7 +886,7 @@ class PlayState extends gapple.MusicBeatState
 		iconP1.y = healthBar.y - (iconP1.height / 2);
 		add(iconP1);
 
-		if (SONG.song.toLowerCase() == 'applecore' && SONG.song.toLowerCase() == 'disruption' && FlxG.save.data.newspritetest) {
+		if (FlxG.save.data.newspritetest) {
 			iconP2 = new HealthIconNew(SONG.player2 == "bambi" ? "bambi-stupid" : SONG.player2, false);
 			iconP2.y = healthBar.y - (iconP2.height / 2);
 			add(iconP2);
